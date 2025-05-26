86a4828613a7c102695d3dd95141dd3e423bf108
martinlarrondo <mblarrondo@gmail.com>
2025-05-26 14:02:13 -0400

    funcion  validar
    crear la funcion validar

--- script2.js
+++ script2.js
@@ -2,6 +2,24 @@
 //validar edad: Debe ser mayor o igual a 18 y menor a 100
 let personas = []
 function validar(){
-    let e
+    let eNombre = document.getElementById("Nombre")
+    let errorNombre = document.getElementById("errorNombre")
+    let vNombre = eNombre.value
+    let eEdad  =document.getElementById("edad")
+    let errorEdad = document.getElementById("errorEdad")
+    let vEdad = eEdad.value
+    let fNombre = validarlargominmo(eNombre,errorNombre,vNombre)
+    let fEdad  =validarlargominmo(eEdad,errorEdad,vEdad)
+    if(fNombre == "exito" && fEdad == "exito"){
+        let p{
+            vNombre:nombre
+            vEdad: edad
+        }
+        
+    }
+
+
+}
+function cargardatos(){
 
 }
